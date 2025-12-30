f8da417c7369da6b29e0d006d76945b6390cbb79
0050505ishan <isan91798@gmail.com>
2025-12-30 20:09:10 +0545

    model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,27 @@
-import { Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
+import { userType } from "../types/user.type";
+const userSchema: Schema = new Schema<userType>(
+    {
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        username: { type: String, required: true, unique: true },
+        firstName: { type: String },
+        lastName: { type: String },
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user',
+        }
+    },
+    {
+        timestamps: true, // auto createdAt and updatedAt
+    }
+);
 
-const userSchema: new Schema = new Schema<UserType>{
-    
+export interface IUser extends userType, Document { // combine UserType and Document
+    _id: mongoose.Types.ObjectId; // mongo related attribute/ custom attributes
+    createdAt: Date;
+    updatedAt: Date;
 }
+
+export const UserModel = mongoose.model<IUser>('User', userSchema);
--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -3,10 +3,10 @@ import z from "zod";
 export const userSchema = z.object({
     username: z.string().min(1),
     email:z.email(),
-    passeord:z.string().min(8),
+    password:z.string().min(8),
     firstName:z.string().optional(),
     lastName:z.string().optional(),
     role: z.enum(["user", "admin"]).default("user"),
 });
 
-export type UserType = z.infer<typeof userSchema>;
+export type userType = z.infer<typeof userSchema>;
