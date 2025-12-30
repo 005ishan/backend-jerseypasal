5a708c9d2c0c82afcef2d6915853a00d47760df3
0050505ishan <isan91798@gmail.com>
2025-12-30 21:07:58 +0545

    auth-controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,48 @@
+import { AuthService } from "../services/auth.service";
+import { createUserDTO, loginUserDTO } from "../dtos/user.dto";
+import { Request, Response } from "express";
+import z from "zod";
+let userService = new AuthService();
 export class AuthController {
-    async register(req: Request, res: Response){
-        
+    async register(req: Request, res: Response) {
+        try {
+            const parsedData = createUserDTO.safeParse(req.body); 
+            if (!parsedData.success) { 
+                return res.status(400).json(
+                    { success: false, message: z.prettifyError(parsedData.error) }
+                )
+            }
+            const userData: createUserDTO = parsedData.data;
+            const newUser = await userService.createUser(userData);
+            return res.status(201).json(
+                { success: true, message: "Registered Successfully", data: newUser }
+            );
+        } catch (error: Error | any) {
+            return res.status(error.statusCode ?? 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            );
+        }
     }
+
+    async login(req: Request, res: Response) {
+        try {
+            const parsedData = loginUserDTO.safeParse(req.body);
+            if (!parsedData.success) {
+                return res.status(400).json(
+                    { success: false, message: z.prettifyError(parsedData.error) }
+                )
+            }
+            const loginData: loginUserDTO = parsedData.data;
+            const { token, user } = await userService.loginUser(loginData);
+            return res.status(200).json(
+                { success: true, message: "Login successfully", data: user, token }
+            );
+
+        } catch (error: Error | any) {
+            return res.status(error.statusCode ?? 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            );
+        }
+    }
+    
 }
